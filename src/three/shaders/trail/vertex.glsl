66adaf6eb3791b18ab3bbc9c6f875fea79d3cc60
Zennyth <matsfox@orange.fr>
2022-02-21 21:01:49 +0100

    fix vertex

--- src/three/shaders/trail/vertex.glsl
+++ src/three/shaders/trail/vertex.glsl
@@ -1,6 +1,25 @@
-attribute vec3 center;
-varying vec3 vCenter;
+attribute vec3 next;
+attribute vec3 prev;
+attribute float side;
+
+uniform vec2 uResolution;
+uniform float uThickness;
+
+vec4 getPosition() {
+  vec2 aspect = vec2(uResolution.x / uResolution.y, 1);
+  // vec2 nextScreen = next.xy * aspect;
+  // vec2 prevScreen = prev.xy * aspect;
+
+  // vec2 tangent = normalize(nextScreen - prevScreen);
+  // vec2 normal = vec2(-tangent.y, tangent.x);
+  // normal /= aspect;
+  // normal *= 0.1;
+
+  vec4 current = vec4(position, 1.);
+  current.xy -= normal.xy / aspect * .1;
+  return current;
+}
+
 void main() {
-  vCenter = center;
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  gl_Position = getPosition();
 }
